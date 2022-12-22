9d6bdb09671471ed49c16d63ab1df94945fee3f1
maycon-santana <maycon.santana@thstec.com.br>
2022-12-22 16:09:49 -0300

    Criando a conexão com o banco MongoDB

--- server/index.js
+++ server/index.js
@@ -8,6 +8,15 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log(`Conexão com Banco MongoDB realizado com sucesso!`);
+}).catch((err) => {
+    console.log(err.message + `Erro ao criar conexão com Banco MongoDB.`);
+});
+
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server Started on Port ${process.env.PORT}`);
 });
